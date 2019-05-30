b5269595c01f3b1be432b9c66b7492c6506e4c69
Evandro Pavei <evandro.pavei@neoway.com.br>
2019-05-30 12:49:01 -0300

    Create tables without gormigrate

--- cmd/migration/main.go
+++ cmd/migration/main.go
@@ -6,11 +6,8 @@ import (
 	"os"
 	"strings"
 
-	"gopkg.in/gormigrate.v1"
-
 	gorsk "github.com/evzpav/gorsk/pkg/utl/model"
 	"github.com/evzpav/gorsk/pkg/utl/secure"
-
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -28,33 +25,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var migrations = []*gormigrate.Migration{}
-
-	m := gormigrate.New(db, gormigrate.DefaultOptions, migrations)
-
-	m.InitSchema(func(tx *gorm.DB) error {
-		err := tx.AutoMigrate(
-			&gorsk.User{},
-			&gorsk.Company{},
-			&gorsk.Location{},
-			&gorsk.Role{},
-			&gorsk.Trade{},
-		).Error
-
-		if err != nil {
-			return err
-		}
-
-		// if err := tx.Model(&gorsk.User{}).AddForeignKey("role_id", "roles (id)", "RESTRICT", "RESTRICT").Error; err != nil {
-		// 	return err
-		// }
-		return nil
-	})
-
-	if err := m.Migrate(); err != nil {
-		log.Fatalf("Could not migrate: %v", err)
-	}
-
+	createSchema(db, &gorsk.User{}, &gorsk.Company{}, &gorsk.Location{}, &gorsk.Role{})
 	dbInsert := `INSERT INTO public.companies VALUES (1, now(), now(), 'admin_company', true);
 	INSERT INTO public.locations VALUES (1, now(), now(), 'admin_location', true, 'admin_address', 1);
 	INSERT INTO public.roles VALUES (100, 100, 'SUPER_ADMIN');
@@ -75,6 +46,18 @@ func main() {
 	userInsert := `INSERT INTO public.users (id, first_name, last_name, username, password, email, active, role_id, company_id, location_id) VALUES (1,'Admin', 'Admin', 'admin', '%s', 'johndoe@mail.com', true, 100, 1, 1);`
 	err = db.Exec(fmt.Sprintf(userInsert, secure.New(1, nil).Hash("admin"))).Error
 	if err != nil {
-		log.Fatalf("Could not migrate: %v", err)
+		log.Fatal(err)
+	}
+
+	log.Println("Initial migration completed")
+
+}
+
+func createSchema(db *gorm.DB, models ...interface{}) {
+	for _, model := range models {
+		err := db.CreateTable(model).Error
+		if err != nil {
+			log.Printf("Error creating table: %+v", err)
+		}
 	}
 }
